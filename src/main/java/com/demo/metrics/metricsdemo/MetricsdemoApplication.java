2ea21e7b1e04b0f3a8aa195e6b18112e6da5e9d7
Sandeep Pandey <spandey@bol.com>
2021-03-05 17:37:29 +0100

    first commit

---
+++ src/main/java/com/demo/metrics/metricsdemo/MetricsdemoApplication.java
@@ -0,0 +1,13 @@
+package com.demo.metrics.metricsdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MetricsdemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MetricsdemoApplication.class, args);
+	}
+
+}
