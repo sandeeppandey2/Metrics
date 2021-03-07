a176c3d79b85d77234f586e8b1c11f6fe001c3be
Sandeep Pandey <spandey@bol.com>
2021-03-07 18:28:42 +0100

    add exception handler

--- src/main/java/com/demo/metrics/metricsdemo/exceptionhandler/ApiError.java
+++ src/main/java/com/demo/metrics/metricsdemo/exceptionhandler/ApiError.java
@@ -9,22 +9,22 @@ import java.util.List;
 @Data
 public class ApiError {
 
-        private HttpStatus status;
-        private String message;
-        private List<String> errors;
+    private HttpStatus status;
+    private String message;
+    private List<String> errors;
 
-        public ApiError(final HttpStatus status, final String message, final List<String> errors) {
-            super();
-            this.status = status;
-            this.message = message;
-            this.errors = errors;
-        }
-
-        public ApiError(final HttpStatus status, final String message, final String error) {
-            super();
-            this.status = status;
-            this.message = message;
-            errors = Arrays.asList(error);
-        }
+    public ApiError(final HttpStatus status, final String message, final List<String> errors) {
+        super();
+        this.status = status;
+        this.message = message;
+        this.errors = errors;
     }
+
+    public ApiError(final HttpStatus status, final String message, final String error) {
+        super();
+        this.status = status;
+        this.message = message;
+        errors = Arrays.asList(error);
+    }
+}
 
